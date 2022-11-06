1dd5da1a31fb41851ce99d7082d17ffc42949dad
lyxlwz <1850115720@qq.com>
2022-11-06 21:17:15 +0800

    简写

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,13 @@
 // 先把所有的工具函数导出的模块在这里导入
 // 然后在统一导出
 import { http } from './http'
-import {
+
+export {
+  http
+}
+
+export {
   setToken,
   getToken,
   removeToken
 } from './token'
-
-export {
-  http,
-  setToken,
-  getToken,
-  removeToken
-}
