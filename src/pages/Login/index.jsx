ed0036d83b9f99d932ca327cedcd4263332a5b13
lyxlwz <1850115720@qq.com>
2022-11-06 15:06:16 +0800

    样式突出

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import { Card } from 'antd'
+import { Card, Button, Checkbox, Form, Input } from 'antd'
 
 import logo from '@/assets/logo.png'
 import './index.scss'
@@ -10,6 +10,24 @@ function Login () {
         <img className='login-logo' src={logo} alt=''></img>
 
         {/* 登录表单 */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+
+          <Form.Item>
+            <Checkbox className="login-checkbox-label">我已阅读并同意「用户协议」和「隐私条款」</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
       </Card>
     </div>
   )
