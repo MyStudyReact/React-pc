0af82ffede102a48c52fdc3b32f2a75a55e10851
lyxlwz <1850115720@qq.com>
2022-11-06 18:02:37 +0800

    路由鉴权实现

---
+++ src/components/AuthComponent.jsx
@@ -0,0 +1,26 @@
+// 1.判断token 是否存在
+// 2.如果存在 直接正常渲染
+// 3.如果不存在 重定向道登录路由
+
+/**
+ * 高阶组件
+ * 把一个组件当成另外一个组件的参数传入
+ * 然后通过一定的判断 返回新的组件
+ */
+import { Navigate } from 'react-router-dom'
+
+import { getToken } from '@/utils'
+
+const AuthComponent = ({ children }) => {
+  const isToken = getToken()
+  if (isToken) {
+    return (<>{children}</>)
+  } else {
+    return (<><Navigate to='/login' replace></Navigate></>)
+  }
+}
+// <AuthComponent> <Layout/> </AuthComponent>
+// 登录：<><Layout/></>
+// 非登录：<Navigate to="/login" replace />
+
+export { AuthComponent }
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,13 +1,20 @@
 import { useRoutes } from 'react-router-dom'
 import { lazy } from "react"
 
+// 引入鉴权组件
+import { AuthComponent } from "@/components/AuthComponent"
+
 const Login = lazy(() => import("@/pages/Login"))
 const Layout = lazy(() => import("@/pages/Layout"))
 
 const routesList = [
+  /**
+   * Layout是需要鉴权处理的,
+   * 这里的Layout不能写死，要根据是否登录进行判断
+   */
   {
     path: '/',
-    element: <Layout />
+    element: <AuthComponent><Layout /></AuthComponent>
   },
   {
     path: '/login',
