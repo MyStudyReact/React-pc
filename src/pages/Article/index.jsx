e61d5945829237f45a1e0d46ba94f688de5e06b2
lyxlwz <1850115720@qq.com>
2022-11-08 13:28:58 +0800

    表格区域结构

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,9 +1,12 @@
 
 import { Link } from 'react-router-dom'
-import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select } from 'antd'
+import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select, Table, Tag, Space } from 'antd'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import 'moment/locale/zh-cn'
 import locale from 'antd/es/date-picker/locale/zh_CN'
+
 import './index.scss'
+import img404 from '@/assets/img/error.png'
 
 const { Option } = Select
 const { RangePicker } = DatePicker
@@ -16,12 +19,81 @@ const initialValues = {
 
 const Article = () => {
 
+  const data = [
+    {
+      id: '8218',
+      comment_count: 0,
+      cover: {
+        images: ['http://geek.itheima.net/resources/images/15.jpg'],
+      },
+      like_count: 0,
+      pubdate: '2019-03-11 09:00:00',
+      read_count: 2,
+      status: 2,
+      title: 'wkwebview离线化加载h5资源解决方案'
+    }
+  ]
+
+  const columns = [
+    {
+      title: '封面',
+      dataIndex: 'cover',
+      width: 120,
+      render: cover => {
+        return <img src={cover || img404} width={80} height={60} alt="" />
+      }
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      width: 220
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      render: data => <Tag color="green">审核通过</Tag>
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'pubdate'
+    },
+    {
+      title: '阅读数',
+      dataIndex: 'read_count'
+    },
+    {
+      title: '评论数',
+      dataIndex: 'comment_count'
+    },
+    {
+      title: '点赞数',
+      dataIndex: 'like_count'
+    },
+    {
+      title: '操作',
+      render: data => {
+        return (
+          <Space size="middle">
+            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+            <Button
+              type="primary"
+              danger
+              shape="circle"
+              icon={<DeleteOutlined />}
+            />
+          </Space>
+        )
+      }
+    }
+  ]
+
   const onFinish = (values) => {
     console.log(values, '==values')
   }
 
   return (
     <div>
+      {/* 筛选区域 */}
       <Card
         title={
           <Breadcrumb separator=">">
@@ -67,6 +139,11 @@ const Article = () => {
             </Button>
           </Form.Item>
         </Form>
+      </Card>
+
+      {/* 文章列表区域 */}
+      <Card title={`根据筛选条件共查询到 count 条结果：`}>
+        <Table rowKey="id" columns={columns} dataSource={data} />
       </Card>
     </div>
   )
